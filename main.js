e334c6637575b384b6be02bde2512f49b0be8bc5
sandipsaha597 <40121135+sandipsaha597@users.noreply.github.com>
2019-06-14 13:58:14 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,41 @@
+'use strict'
+
+// animated icon 1 -start
+let icon1 = document.querySelector('.animated-icon1')
+
+icon1.onclick = function() {
+  icon1.classList.toggle('active')
+}
+// animated icon 1 -end
+
+// animated icon 2 -start
+let icon2 = document.querySelector('.animated-icon2')
+
+icon2.onclick = function() {
+  icon2.classList.toggle('active')
+}
+// animated icon 2 -end
+
+// animated icon 3 -start
+let icon3 = document.querySelector('.animated-icon3')
+
+icon3.onclick = function() {
+  icon3.classList.toggle('active')
+}
+// animated icon 3 -end
+
+// animated icon 4 -start
+let icon4 = document.querySelector('.animated-icon4')
+
+icon4.onclick = function() {
+  icon4.classList.toggle('active')
+}
+// animated icon 4 -end
+
+// animated icon 5 -start
+let icon5 = document.querySelector('.animated-icon5')
+
+icon5.onclick = function() {
+  icon5.classList.toggle('active')
+}
+// animated icon 5 -end
